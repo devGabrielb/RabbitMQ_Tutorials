ac41f274d32dbd0febf882b25f6a56e08271c16a
Gabriel Batista da Silva <gabriel.batista@take.net>
2022-06-16 22:23:51 -0300

    RabbitMQ - Hello World Tutorial

---
+++ hello_world/Receive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using static System.Console;
+
+namespace Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    WriteLine("[X] Received {0}", message);
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer
+                );
+                WriteLine("Press [Enter] to exit");
+                ReadLine();
+            }
+        }
+    }
+}
